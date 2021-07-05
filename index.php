306ed4ef5064ebf3feeea7921774f87bde690f45
String03 <40239185+String03@users.noreply.github.com>
2021-07-04 21:03:37 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    
+<?php
+    echo "Bienvenido al sistema";
+ 
+    echo "<br>";
+
+    echo "Nombre: Ana";
+
+    echo "<br>";
+
+    echo "Apellido: Laura";
+
+    echo "<br>";
+
+    echo "Edad: 27";
+
+    echo "<br>";
+
+    echo "Editor de código: Visual Code";
+
+
+
+
+?>
+
+
+</body>
+</html>
